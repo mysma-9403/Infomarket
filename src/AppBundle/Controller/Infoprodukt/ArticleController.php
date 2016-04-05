7a0888e5ab57642a09bdae69c7f630a8ab656dde
in3orn <in3orn@gmail.com>
2016-04-05 11:11:39 +0200

    InfoProdukt - video
    Dalsze zmiany

---
+++ src/AppBundle/Controller/Infoprodukt/ArticleController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AppBundle\Controller\Infoprodukt;
+
+use AppBundle\Controller\Infoprodukt\Base\SimpleEntityController;
+use AppBundle\Entity\Article;
+use AppBundle\Entity\Filter\Base\SimpleEntityFilter;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ArticleController extends SimpleEntityController
+{   
+	/**
+	 * 
+	 * @param Request $request
+	 */
+	public function indexAction(Request $request, $page)
+	{
+		return $this->indexActionInternal($request, $page);
+	}
+	
+	/**
+	 * 
+	 * @param Request $request
+	 * @param unknown $id
+	 */
+	public function showAction(Request $request, $id)
+	{
+		return $this->showActionInternal($request, $id);
+	}
+	
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * @see \AppBundle\Controller\Infomarket\Base\BaseEntityController::getEntityType()
+	 */
+    protected function getEntityType()
+    {
+    	return Article::class;
+    }
+
+    /**
+     * 
+     * {@inheritDoc}
+     * @see \AppBundle\Controller\Infomarket\Base\BaseEntityController::getEntityFilter()
+     */
+    protected function getEntityFilter(Request $request)
+    {
+    	$filter = new SimpleEntityFilter();
+    	$filter->setPublished(true);
+    	
+    	return $filter;
+    }
+}
