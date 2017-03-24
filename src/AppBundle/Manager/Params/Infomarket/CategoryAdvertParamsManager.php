f22106d8e26acbc23b55e47cbb4270660a6f480a
in3orn <in3orn@gmail.com>
2017-03-24 17:53:23 +0100

    IM Category adverts bugfix
    For index categories - entry is null -> use branch context categories

--- src/AppBundle/Manager/Params/Infomarket/CategoryAdvertParamsManager.php
+++ src/AppBundle/Manager/Params/Infomarket/CategoryAdvertParamsManager.php
@@ -10,17 +10,22 @@ class CategoryAdvertParamsManager extends AdvertParamsManager {
 	
 	public function __construct($doctrine, array $advertLocations) {
 		parent::__construct($doctrine, $advertLocations);
-		$this->checkInRoots = false;
 	}
 	
 	protected function getContextCategories(Request $request, $contextParams, $viewParams) {
 		$category = $request->get('id');
 		
-		$em = $this->doctrine->getManager();
-		$categoryRepository = new CategoryRepository($em, $em->getClassMetadata(Category::class));
-		
-		$categories = $categoryRepository->findContextParents($category);
-		$categories = array_merge($categories, $categoryRepository->findContextChildren($category));
+		if($category) {
+			$em = $this->doctrine->getManager();
+			$categoryRepository = new CategoryRepository($em, $em->getClassMetadata(Category::class));
+			
+			$categories = $categoryRepository->findContextParents($category);
+			$categories = array_merge($categories, $categoryRepository->findContextChildren($category));
+			$this->checkInRoots = false;
+		} else {
+			$categories = parent::getContextCategories($request, $contextParams, $viewParams);
+			$this->checkInRoots = true;
+		}
 		
 		return $categories;
 	}
