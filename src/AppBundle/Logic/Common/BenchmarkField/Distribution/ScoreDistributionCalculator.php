9c99bd042bf3a81fefc47e0aa736e8ee0c389b81
in3orn <in3orn@gmail.com>
2017-12-03 10:48:57 +0100

    ProductNotes update - batch processing - part. 4

--- src/AppBundle/Logic/Common/BenchmarkField/Distribution/ScoreDistributionCalculator.php
+++ src/AppBundle/Logic/Common/BenchmarkField/Distribution/ScoreDistributionCalculator.php
@@ -7,30 +7,30 @@ use AppBundle\Entity\Main\BenchmarkField;
 use AppBundle\Entity\Main\Category;
 
 class ScoreDistributionCalculator {
-	
+
 	/**
 	 *
 	 * @var BenchmarkFieldDataBaseUtils
 	 */
 	private $benchmarkFieldDataBaseUtils;
-	
+
 	/**
-	 * 
+	 *
 	 * @var DistributionMerger
 	 */
 	private $distributionMerger;
-	
-	public function __construct(BenchmarkFieldDataBaseUtils $benchmarkFieldDataBaseUtils,
+
+	public function __construct(BenchmarkFieldDataBaseUtils $benchmarkFieldDataBaseUtils, 
 			DistributionMerger $distributionMerger) {
 		$this->benchmarkFieldDataBaseUtils = $benchmarkFieldDataBaseUtils;
 		$this->distributionMerger = $distributionMerger;
 	}
-	
+
 	public function calculate(BenchmarkField $field) {
 		$category = $field->getCategory();
 		
 		$result = $this->calculateForCategory($category, $field);
-	
+		
 		/** @var Category $subcategory */
 		foreach ($category->getChildren() as $subcategory) {
 			if ($subcategory->getBenchmark()) {
@@ -38,29 +38,31 @@ class ScoreDistributionCalculator {
 				$result = $this->distributionMerger->merge($result, $subresult);
 			}
 		}
-	
+		
 		ksort($result);
 		return $result;
 	}
-	
+
 	private function calculateForCategory(Category $category, BenchmarkField $field) {
 		$result = [];
-	
+		
 		/** @var ProductCategoryAssignment $assignment */
 		foreach ($category->getProductCategoryAssignments() as $assignment) {
 			$productScore = $assignment->getProductScore();
-			$offset = $this->benchmarkFieldDataBaseUtils->getScoreField($field);
-			$value = $productScore->offsetGet($offset);
+			if ($productScore) {
+				$offset = $this->benchmarkFieldDataBaseUtils->getScoreField($field);
+				$value = $productScore->offsetGet($offset);
 				
-			if ($value !== null) {
-				if (key_exists($value, $result)) {
-					$result[$value] = $result[$value] + 1;
-				} else {
-					$result[$value] = 1;
+				if ($value !== null) {
+					if (key_exists($value, $result)) {
+						$result[$value] = $result[$value] + 1;
+					} else {
+						$result[$value] = 1;
+					}
 				}
 			}
 		}
-	
+		
 		return $result;
 	}
 }
