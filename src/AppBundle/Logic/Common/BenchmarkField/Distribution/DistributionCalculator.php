43e3e889e2c78e8a0c1394ac53759b9ca1d3c3ce
in3orn <in3orn@gmail.com>
2017-12-03 10:47:25 +0100

    ProductNotes update - batch processing - part. 3

--- src/AppBundle/Logic/Common/BenchmarkField/Distribution/DistributionCalculator.php
+++ src/AppBundle/Logic/Common/BenchmarkField/Distribution/DistributionCalculator.php
@@ -7,30 +7,30 @@ use AppBundle\Entity\Main\BenchmarkField;
 use AppBundle\Utils\Entity\DataBase\BenchmarkFieldDataBaseUtils;
 
 class DistributionCalculator {
-	
+
 	/**
 	 *
 	 * @var BenchmarkFieldDataBaseUtils
 	 */
 	private $benchmarkFieldDataBaseUtils;
-	
+
 	/**
-	 * 
+	 *
 	 * @var DistributionMerger
 	 */
 	private $distributionMerger;
-	
-	public function __construct(BenchmarkFieldDataBaseUtils $benchmarkFieldDataBaseUtils,
+
+	public function __construct(BenchmarkFieldDataBaseUtils $benchmarkFieldDataBaseUtils, 
 			DistributionMerger $distributionMerger) {
 		$this->benchmarkFieldDataBaseUtils = $benchmarkFieldDataBaseUtils;
 		$this->distributionMerger = $distributionMerger;
 	}
-	
+
 	public function calculate(BenchmarkField $field) {
 		$category = $field->getCategory();
 		
 		$result = $this->calculateForCategory($category, $field);
-	
+		
 		/** @var Category $subcategory */
 		foreach ($category->getChildren() as $subcategory) {
 			if ($subcategory->getBenchmark()) {
@@ -38,41 +38,43 @@ class DistributionCalculator {
 				$result = $this->distributionMerger->merge($result, $subresult);
 			}
 		}
-	
+		
 		ksort($result);
 		return $result;
 	}
-	
+
 	private function calculateForCategory(Category $category, BenchmarkField $field) {
 		$result = [];
-	
+		
 		/** @var ProductCategoryAssignment $assignment */
 		foreach ($category->getProductCategoryAssignments() as $assignment) {
 			$productValue = $assignment->getProductValue();
-			$offset = $this->benchmarkFieldDataBaseUtils->getValueField($field);
-			$value = $productValue->offsetGet($offset);
+			if ($productValue) {
+				$offset = $this->benchmarkFieldDataBaseUtils->getValueField($field);
+				$value = $productValue->offsetGet($offset);
 				
-			if ($value !== null) {
-				if ($field->getFieldType() == BenchmarkField::MULTI_ENUM_FIELD_TYPE) {
-					$enums = explode(",", $value);
-					$enums = array_filter($enums, 'trim');
-					foreach ($enums as $enum) {
-						if (key_exists($enum, $result)) {
-							$result[$enum] = $result[$enum] + 1;
-						} else {
-							$result[$enum] = 1;
+				if ($value !== null) {
+					if ($field->getFieldType() == BenchmarkField::MULTI_ENUM_FIELD_TYPE) {
+						$enums = explode(",", $value);
+						$enums = array_filter($enums, 'trim');
+						foreach ($enums as $enum) {
+							if (key_exists($enum, $result)) {
+								$result[$enum] = $result[$enum] + 1;
+							} else {
+								$result[$enum] = 1;
+							}
 						}
-					}
-				} else {
-					if (key_exists($value, $result)) {
-						$result[$value] = $result[$value] + 1;
 					} else {
-						$result[$value] = 1;
+						if (key_exists($value, $result)) {
+							$result[$value] = $result[$value] + 1;
+						} else {
+							$result[$value] = 1;
+						}
 					}
 				}
 			}
 		}
-	
+		
 		return $result;
 	}
 }
