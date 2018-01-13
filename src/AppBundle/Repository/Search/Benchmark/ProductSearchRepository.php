c1ad2e2018a4a38ec5616444f0cb7ffd53b41c39
in3orn <in3orn@gmail.com>
2018-01-13 14:03:42 +0100

    Benchmark search - exclude products from not finished categories

--- src/AppBundle/Repository/Search/Benchmark/ProductSearchRepository.php
+++ src/AppBundle/Repository/Search/Benchmark/ProductSearchRepository.php
@@ -10,12 +10,14 @@ use AppBundle\Repository\Search\Base\SearchRepository;
 use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\ORM\QueryBuilder;
 use AppBundle\Entity\Assignments\ProductCategoryAssignment;
+use AppBundle\Entity\Main\Category;
 
 class ProductSearchRepository extends SearchRepository {
 
 	protected function buildJoins(QueryBuilder &$builder, Filter $filter) {
 		$builder->innerJoin(Brand::class, 'b', Join::WITH, 'b.id = e.brand');
 		$builder->innerJoin(ProductCategoryAssignment::class, 'pca', Join::WITH, 'pca.product = e.id');
+		$builder->innerJoin(Category::class, 'c', Join::WITH, 'c.id = pca.category');
 	}
 
 	protected function getSelectFields(QueryBuilder &$builder, Filter $filter) {
@@ -35,6 +37,7 @@ class ProductSearchRepository extends SearchRepository {
 		$where = $expr->andX();
 		
 		$where->add($expr->eq('e.benchmark', 1));
+		$where->add($expr->eq('c.inProgress', 0));
 		
 		if (count($filter->getCategories()) > 0) {
 			$where->add($expr->in('pca.category', $filter->getCategories()));
