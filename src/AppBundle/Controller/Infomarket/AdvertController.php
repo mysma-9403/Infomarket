28777227ba679702efba62960d639e0067349d28
in3orn <in3orn@gmail.com>
2018-10-01 20:00:07 +0200

    Allow newsletter ads to use different http scheme

--- src/AppBundle/Controller/Infomarket/AdvertController.php
+++ src/AppBundle/Controller/Infomarket/AdvertController.php
@@ -11,14 +11,14 @@ class AdvertController extends DummyController {
 	// ---------------------------------------------------------------------------
 	// Actions
 	// ---------------------------------------------------------------------------
-	public function clickAction(Request $request, $id) {
-		return $this->clickActionInternal($request, $id);
+	public function clickAction(Request $request, $id, $replaceScheme) {
+		return $this->clickActionInternal($request, $id, $replaceScheme);
 	}
 	
 	// ---------------------------------------------------------------------------
 	// Internal actions
 	// ---------------------------------------------------------------------------
-	protected function clickActionInternal(Request $request, $id) {
+	protected function clickActionInternal(Request $request, $id, $replaceScheme) {
 		$am = $this->getAnalyticsManager();
 		$am->sendEventAnalytics($this->getEntityName(), 'click', $id);
 		
@@ -27,13 +27,17 @@ class AdvertController extends DummyController {
 		
 		$this->saveEntry($entry);
 		
-		$scheme = $request->getScheme() . '://';
+		if ($replaceScheme == 1) {
+			$scheme = $request->getScheme() . '://';
+			
+			$link = $entry->getLink();
+			$link = str_replace('http://', '', $link);
+			$link = str_replace('https://', '', $link);
+			
+			return $this->redirect($scheme . $link);
+		}
 		
-		$link = $entry->getLink();
-		$link = str_replace('http://', '', $link);
-		$link = str_replace('https://', '', $link);
-		
-		return $this->redirect($scheme . $link);
+		return $this->redirect($entry->getLink());
 	}
 
 	protected function saveEntry($entry) {
