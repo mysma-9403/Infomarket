458cc403700a6d66af034dc9ed1581806db72352
in3orn <in3orn@gmail.com>
2019-01-05 11:54:37 +0100

    Reduce usage of style attribute in html body
    + Remove caching on dev (for testing)

--- web/app_dev.php
+++ web/app_dev.php
@@ -26,9 +26,9 @@ Debug::enable();
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
-$kernel = new AppCache($kernel);
+// $kernel = new AppCache($kernel);
 
-Request::enableHttpMethodParameterOverride();
+// Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
